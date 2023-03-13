1bece57f081af62f3aebfdab3d92335b556d8a5d
cherdianto <candraherdianto90@gmail.com>
2023-03-13 23:10:44 +0100

    add form berkah ramadhan

--- controllers/imeaController.js
+++ controllers/imeaController.js
@@ -71,6 +71,82 @@ const imeaGreeting = asyncHandler(async(req, res) => {
     
 })
 
+const formBerkah = asyncHandler(async(req, res) => {
+    const device = req.device
+    const user = req.user
+    const cronTask = req.cronTask
+
+    const {
+        to = req.query.to,
+        // message = req.query.message,
+        isGroup = req.query.isGroup,
+        ref_id = req.query.ref_id,
+        retry = req.query.retry,
+        priority = req.query.priority,
+        nama= req.query.nama,
+        porsi= req.query.porsi
+    } = req.body
+
+
+
+    const text = `Bismillah\n\nKak *${nama.toUpperCase()}*\n${porsi}\n\nTerimakasih telah mengisi form keikutsertaan acara IMEA Gabungan edisi *Ngabuburit Bersama IMEA* 25 Maret 2023
+        Data Kakak sudah kami simpan.\n\nKami tunggu kehadiran Kakak (beserta keluarga) di acara tsb
+        \nHari/Tanggal : Sabtu, 25 Maret 2023\nJam : 16.30 - 18.30 CET\nTempat : Ruang De Globe, ITC International Hotel\nAlamat : Boulevard 1945-4, 7511AE Enschede\n(https://goo.gl/maps/sxwnE4dkPQDq7kzz8)\nSusunan Acara :\n- 16.30 Pembukaan\n- 16.45 Tausiyah\n- 17.30 Sambut warga baru & sosialisasi program IMEA 2023\n- 18.15 Pembagian Berkah Ramadhan (Ta'jil)\n- 18.30 Penutup\n\nCatatan :\n~ Mohon untuk datang tepat waktu ya.\n~ Bagi yg memiliki gejala flu, dipersilakan untuk beristirahat di rumah\n~ Parkir sepeda di parkiran MST (https://maps.app.goo.gl/AJo4nJuRDzL2GTLK9)\n\nTerimakasih\n\n*INFO DONASI UNTUK BUKA PUASA*\n\nIMEA membuka donasi khusus untuk program berkah ramadhan ini. InsyaAllah semua donasi yang masuk akan dibagikan ke warga IMEA dalam bentuk ta'jil (makanan buka puasa). 
+        Donasi bisa di transfer melalui link Tikkie di bawah ini\n
+        https://tikkie.me/pay/77up48vdideuomij4eaf\n\n
+        Jika menghendaki untuk menyumbang dalam bentuk *makanan*, silakan hubungi Kak Fella\n
+        https://wa.me/+3548551240\n\n
+        Jika menghendaki untuk berkontribusi dalam bentuk lain, silakan membalas pesan ini\n\n
+        Semoga Allah Azza wa Jalla menerima amal kita dan membalas dengan pahala yang berlipat. Aamiin\n\n
+        *“Barang siapa memberi makanan untuk berbuka kepada orang yang berpuasa, maka ia akan mendapatkan pahala orang yang berpuasa itu tanpa dikurangi sedikitpun.”* (HR. Ahmad, Nasa’i dan dishahihkan oleh Al Albani)`
+
+    if(!to) {
+        res.status(400)
+        throw new Error("TO_REQUIRED")
+    }
+
+    if(!device.status === true){
+        res.status(400)
+        throw new Error("INACTIVE_DEVICE")
+    }
+
+    const sendMsg = await Message.create(
+        {   
+            userId: user._id,
+            deviceId: device._id,
+            message: {
+                text : text
+            },
+            to,
+            isGroup,
+            retry,
+            priority,
+            ref_id
+        }
+    )
+    
+    if(!sendMsg){
+        res.status(500)
+        throw new Error("CREATE_MESSAGE_TASK_FAILED")
+    }
+
+    await Device.findByIdAndUpdate(
+        {_id: device._id},
+        { $set: { cronIdle: false } }
+    )
+    
+    console.log('cron start ' + device._id)
+    cronTask[device.id].start()
+
+    res.status(200).json({
+        status: true,
+        message: "CREATE_MESSAGE_TASK_SUCCESS",
+        sendMsg
+    })
+    
+})
+
 export {
-    imeaGreeting
+    imeaGreeting,
+    formBerkah
 }
--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { addMessage, queuMessage, getMessages } from '../controllers/messageController.js'
-import { imeaGreeting } from '../controllers/imeaController.js'
+import { formBerkah, imeaGreeting } from '../controllers/imeaController.js'
 import verifyApiKey from '../middlewares/verifyApiKey.js'
 import verifyToken from '../middlewares/verifyToken.js'
 // const verifyToken = require('../middlewares/verifyToken')
@@ -10,5 +10,6 @@ router.post('/add', verifyApiKey, addMessage)
 router.get('/all', verifyToken, getMessages)
 router.get('/queu', verifyApiKey, queuMessage)
 router.post('/imea-greeting', verifyApiKey, imeaGreeting)
+router.post('/form-berkah', verifyApiKey, formBerkah)
 
 export default router
